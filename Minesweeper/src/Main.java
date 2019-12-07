6c573ffa322b081942655e76f7c156c65818e2b8
sishomit <58613875+sishomit@users.noreply.github.com>
2019-12-07 08:10:33 +0600

    Add files via upload

---
+++ Minesweeper/src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * main class
+ */
+
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // create new object and play
+        Minesweeper ob=new Minesweeper();
+    }
+
+}
